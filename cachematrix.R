605967bdad76516d861660cbd540bcac17388da2
G S <gis@navingio.net>
2017-09-13 22:52:36 +0200

    First version

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,80 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R Programming course - Week 3
+## Peer-graded Assignment: Programming Assignment 2: Lexical Scoping
+## G.Scotti
 
-## Write a short comment describing this function
+## This module defines two functions providing an efficient mechanism for
+## calculating the inverse of a given n*n invertible matrix, caching the result, and make it
+## further available to the application, if required, saving computation time.
 
+## makeCacheMatrix
+##
+## This function gets an n*n invertible matrix as input x, stores a copy of it
+## in the global environment and returns a special "matrix" object that is
+## a list of.. functions. The following:
+##
+## get() : return the stored matrix x
+##
+## setinverse(y) : store the value of y, y being a matrix n*n, 
+##  inverse of the stored matrix x 
+##
+## getinverse() : returns the value of the inverse matrix, stored with setinverse(y)
+##
+## Additionally this method is provided to store a new matrix, if needed
+## set(x): store the value of x, where x is an n*n invertible matrix
+##
 makeCacheMatrix <- function(x = matrix()) {
 
+    i <- NULL
+    
+    ## define methods:
+    
+    set <- function(y) {
+        
+        ## the matrix x is stored in the global environment using the 
+        ##  superassignment operator <<-
+        x <<- y
+        
+        ## similarly, the value of the inverse 'i' is initialised to NULL, 
+        # again in the global environment
+        i <<- NULL
+    }
+    
+    ## get the stored matrix
+    get <- function() x
+    
+    ## The inverse matrix inv is stored in the variable 'i' (global environment)
+    setinverse <- function(inv) i <<- inv
+    
+    ## get the stored inverse matrix
+    getinverse <- function() i
+    
+    
+    ## create and return a list containing the methods defined above 
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
 
+## cacheSolve
+##
+## This function computes the inverse of x, a special "matrix" object returned by 
+## makeCacheMatrix above.
+## The function computes the inverse of x just once, the first time it is called,
+## the inverse is cached for reuse.
+## subsequent calls to this functions returns the cached inverse matrix
+## avoiding to recalculate it again.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+    i <- x$getinverse()
+    if(!is.null(i)) {
+        message("getting cached data")
+        return(i)
+    }
+    data <- x$get()
+
+    i <- solve(data, ...)
+    x$setinverse(i)
+    i
 }
